fe3d0d3feb73760f736629d5131b4663b1744898
Bóris Marin <borismarin@gmail.com>
2023-06-20 20:02:31 -0300

    exemplo de uso calibra

---
+++ eletronica/motor_passo_potenciometro/experimento.py
@@ -0,0 +1,14 @@
+import time
+from apontador import Apontador
+
+def importa_sons(quem):
+    pass
+
+
+nome = 'Joao Bobao'
+sons = importa_sons(nome)
+
+
+with Apontador() as a:
+    print(a.calibra())
+
